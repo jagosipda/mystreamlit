915a891e417f2074d6e926ac378425fdfc8646ff
GOEUN <125917117+jagosipda@users.noreply.github.com>
2023-05-16 12:48:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st 
 import unmpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("Map")
 df = pd.DataFrame(np.random.randn(500,2) / [50, 50] + [37.76, -122.4],columns=['lat','lon'])
 st.map(df)
+
+rand = np.random.normal( 1, 2, size=20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins = 15)
+st.pyplot(fig)
